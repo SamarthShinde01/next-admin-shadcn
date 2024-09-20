967a6db57cbcb64b9dad88a8c4f8c49983cbc5dd
Samarth Shinde <samarthsshinde@gmail.com>
2024-09-20 21:54:58 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { DashboardCard } from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, Users } from "lucide-react";
+
+export default function Home() {
+	return (
+		<>
+			<div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+				<DashboardCard
+					title="Posts"
+					count={100}
+					icon={<Newspaper className="text-slate-500 " size={45} />}
+				/>
+				<DashboardCard
+					title="Categories"
+					count={15}
+					icon={<Folder className="text-slate-500 " size={45} />}
+				/>
+				<DashboardCard
+					title="Users"
+					count={750}
+					icon={<Users className="text-slate-500 " size={45} />}
+				/>
+				<DashboardCard
+					title="Comments"
+					count={1200}
+					icon={<MessageCircle className="text-slate-500 " size={45} />}
+				/>
+			</div>
+		</>
+	);
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/logo.png";
+import { Avatar, AvatarImage, AvatarFallback } from "../components/ui/avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+export const Navbar = () => {
+	return (
+		<div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+			<Link href="/">
+				<Image alt="logos" src={logo} width={40} />
+				{/* <Avatar>
+					<AvatarFallback className="text-black">Logo</AvatarFallback>
+				</Avatar> */}
+			</Link>
+
+			<DropdownMenu>
+				<DropdownMenuTrigger className="focus:outline-none">
+					<Avatar>
+						<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+						<AvatarFallback className="text-black">BT</AvatarFallback>
+					</Avatar>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<Link href="/profile">Profile</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Link href="/logout">Logout</Link>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+};
